b24d771c60cf88eb6723a23f86a997c11da49673
Dmitry <tegorkrsk@gmail.com>
2025-05-12 01:03:27 +0300

    Add ocr

--- ocr/easyocr_script.py
+++ ocr/easyocr_script.py
@@ -1,6 +1,6 @@
 import easyocr
 
 reader = easyocr.Reader(['en', 'ru'])
-result = reader.readtext('fsk.jpg', detail=0, paragraph=True)
+result = reader.readtext('ocr/fsk.jpg', detail=0, paragraph=True)
 with open("ocr_result.txt", "w", encoding="utf-8") as f:
     f.write("\n".join(result))
